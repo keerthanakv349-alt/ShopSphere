9813d41f7528a97e179528da89d0e7bac39c1389
Keerthana <keerthanakv.acumenvelocity@gmail.com>
2026-08-25 16:49:06 +0530

    bug fix

--- frontend/components/AppHeader.tsx
+++ frontend/components/AppHeader.tsx
@@ -1,17 +1,3 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import { SiteHeader } from "./SiteHeader";
-
-// export function AppHeader() {
-//   const pathname = usePathname();
-
-//   if (pathname.startsWith("/admin")) {
-//     return null;
-//   }
-
-//   return <SiteHeader />;
-// }
 
 
 
